b0e3cac3deda60d8d755fea577f15593c5e88c36
Simon <simon@elisya.org>
2023-11-08 11:05:44 +0000

    problem wih list adaptive traits

--- R/computerate.R
+++ R/computerate.R
@@ -14,17 +14,26 @@
 #' lambda(1, 0.3, c(1, 1, 1))
 #' @export
 lambda <- function(base_rate,bonus_rate,advantageous_traits){
-                base_rate+bonus_rate*sum(advantageous_traits) #birth rate wrt to community adaptive traits
+    base_rate+bonus_rate*sum(advantageous_traits) #birth rate wrt to community adaptive traits
 }
 
 
-##test
-lambdas = sapply(seq(0,0.2,.01),lambda,base_rate=0.01,advantageous_traits=c(1, 1, 1))
-plot(seq(0,0.2,.01),lambdas,type="n",xlab="base rate",ylab="bonus rate")
-advs=list(c(0,0,0),c(0,0,1),c(0,1,1),c(1,1,1))
+testLambada <- function(){
+    lambdas = sapply(seq(0,0.2,.01),lambda,base_rate=0.01,advantageous_traits=c(1, 1, 1))
+    plot(seq(0,0.2,.01),lambdas,type="n",xlab="base rate",ylab="bonus rate")
+    advs=list(c(0,0,0),c(0,0,1),c(0,1,1),c(1,1,1))
 
-for(av in advs){
-    lambdas = sapply(seq(0,0.2,.01),lambda,base_rate=0.01,advantageous_traits=av)
-    points(seq(0,0.2,.01),lambdas)
+    for(av in advs){
+        lambdas = sapply(seq(0,0.2,.01),lambda,base_rate=0.01,advantageous_traits=av)
+        points(seq(0,0.2,.01),lambdas)
+    }
+}
+
+notes  <- function(a){
+## notes for simom doing unlist on vector can slower things down ( 4time slower than when doing it with small vector)
+    ## new adatpive trais frunciton that unsure no list are returned
+    alist=lapply(1:1000000,function(a)runif(1))
+    avec=unlist(alist)
+    microbenchmark::microbenchmark(sum(avec),sum(unlist(avec)),sum(unlist(alist)))
 }
 
