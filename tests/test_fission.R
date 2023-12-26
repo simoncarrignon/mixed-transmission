80da4b0ba91703b92218679f9a6349a858d39b6e
Simon <simon@elisya.org>
2023-12-26 17:24:05 +0000

    add tests for fissions process

--- tests/test_fission.R
+++ tests/test_fission.R
@@ -19,3 +19,44 @@ replicate(100,{
               plot(newcom$coordinates,ylim=c(0,mx),xlim=c(0,mx),pch=20,cex=4)
 })
                     })
+
+testthat::test_that("test filling the grid",
+                    {
+                        replicate(5,{
+                                      K=1
+                                      mx=sample(1:15,1)
+                                      pos=random2Dgrid(K=K,Gx=mx)
+                                      a=initAdaptiveTraits(ki=K,km=0,n=10)
+                                      initcomus=initialiseCommunities(traits=a,coordinates=pos,G=mx)
+                                      initcomus$size=rep(10/K,K)
+                                      new.comus=initcomus
+                                      for(i in 1:((mx*mx)-1)){
+                                          Ki=nrow(new.comus$coordinates)
+                                          ol=sample.int(Ki,1)
+                                          new.comus=fissionCommunity(new.comus,ol)
+                                          testthat::expect_equal(Ki,i)
+                                      }
+                                      new.comus=fissionCommunity(new.comus,ol)
+                                      testthat::expect_equal(sum(new.comus$occupation),mx*mx)
+
+})
+                    })
+testthat::test_that("test randomly adding element to the grid",
+                    {
+replicate(50,{
+              ki=sample(2:6,1)
+              km=sample(2:6,1)
+              mx=sample((ki+km):100,1)
+              pos=random2Dgrid(K=ki+km,Gx=mx)
+              a=initAdaptiveTraits(ki=ki,km=km,n=10)
+              initcomus=initialiseCommunities(traits=a,coordinates=pos,G=mx)
+              new.comus=initcomus
+              for(i in 1:5){
+                  Ki=nrow(new.comus$coordinates)
+                  ol=sample.int(Ki,1)
+                  new.comus=fissionCommunity(new.comus,ol)
+                  testthat::expect_equal(Ki,ki+km+i-1)
+              }
+
+                })
+})
