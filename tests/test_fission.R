097460db85fa755490a2eb2cd61fa28ae2b8dc3f
Simon <simon@elisya.org>
2023-12-26 17:58:59 +0000

    add a general fssion with reassign

--- tests/test_fission.R
+++ tests/test_fission.R
@@ -60,3 +60,40 @@ replicate(50,{
 
                 })
 })
+
+neutraltraitsParam=initNeutralTraitsPathways(z = 5)
+percomu=sample(1:100,1)
+K=sample(2:8,1)
+km=round(K/3)
+ki=K-km
+N=K*percomu
+pos=random2Dgrid(K=K,Gx=100)
+a=initAdaptiveTraits(ki=ki,km=km,n=20 )
+initcomus=initialiseCommunities(traits=a,coordinates=pos)
+initcomus$size=rep(percomu,K)
+communities=unlist(lapply(1:K,function(i)rep(i,initcomus$size[i])))
+population=cbind(newpop(N,age="random",community = communities),initNeutralTraits(N,z))
+table(reassignFamiliesToNewCommunityNoFIDs(2,population,200,-1)[,"community"])
+
+z=sample(2:20,1)
+neutraltraitsParam=initNeutralTraitsPathways(z = z)
+neutraltraitsParam$post[,"o"]=rbinom(z,1,.5)
+neutraltraitsParam$post[,"i"]=rbinom(z,1,.5)
+neutraltraitsParam$post[,"h"]=rbinom(z,1,.5)
+neutraltraitsParam$pre[,"o"]=rbinom(z,1,.5)
+neutraltraitsParam$pre[,"v"]=rbinom(z,1,.5)
+neutraltraitsParam$pre[,"h"]=rbinom(z,1,.5)
+neutraltraitsParam$s=rbinom(z,1,.5)
+traitsid=paste0("t",1:z)
+percomu=sample(1:100,1)
+K=sample(2:8,1)
+km=round(K/3)
+ki=K-km
+N=K*percomu
+pos=random2Dgrid(K=K,Gx=10)
+a=initAdaptiveTraits(ki=ki,km=km,n=20 )
+initcomus=initialiseCommunities(traits=a,coordinates=pos,G=10)
+initcomus$size=rep(percomu,K)
+communities=unlist(lapply(1:K,function(i)rep(i,initcomus$size[i])))
+population=cbind(newpop(N,age="random",community = communities),initNeutralTraits(N,z))
+quickV=modelVector(K=K, m=1, b=0.2, r=0.005, rho=0, d=0.01, maturity=18, endrepro=65, population=population, comus=initcomus, tstep=150, tp=neutraltraitsParam,age.threshold=20, out=c("finalpop"),logging=c("visu","fission"),ma=1,traitsid=paste0("t",1:z),F_Th = 20)
