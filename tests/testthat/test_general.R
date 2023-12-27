8f0a2bd91a6798e791caa887b66850d1d256a872
Simon <simon@elisya.org>
2023-12-27 21:58:46 +0000

    minor edit to general test unit

--- tests/testthat/test_general.R
+++ tests/testthat/test_general.R
@@ -12,6 +12,7 @@ endrepro=20
 tstep=20*10
 generation.threshold = 20
 z=5
+traitsid=paste0("t",1:z)
 
 
 
@@ -26,7 +27,7 @@ neutraltraitsParam$post[,"h"]=c(0,0,1,1,1)
 
 pos=random2Dgrid(K=K,Gx=100)
 a=initAdaptiveTraits(ki=ki,km=km)
-initcomus=initialiseCommunities(traits=a,coordinates=pos)
+initcomus=initialiseCommunities(traits=a,coordinates=pos,G=100)
 initcomus$size=rep(N/K,K)
 
 communities=unlist(lapply(1:K,function(i)rep(i,initcomus$size[i])))
@@ -51,7 +52,6 @@ testthat::test_that('general test',{
 
 testthat::test_that('general test',{
                             quickV=modelVector(K=K, m=1, b=0, r=0, rho=1, d=0, maturity=50, endrepro=60, population=population, comus=initcomus, tstep=50, tp=neutraltraitsParam,age.threshold=generation.threshold, out="finalpop",logging=NULL,ma=1,traitsid=paste0("t",1:z))
-traitsid=paste0("t",1:z)
 testthat::expect_true(all( apply(quickV$population[,traitsid],2,sum)[1:2]== apply(population[,traitsid],2,sum)[1:2]))
                     })
 
@@ -66,7 +66,7 @@ testthat::test_that("model don't change when no growth, no social learning",{
                         replicate(5,
                                   {
 
-                                      quickV=modelVector(K=K, m=1, b=0, r=0, rho=1, d=0, maturity=sample(100,1), endrepro=sample(100,1), population=population, comus=initcomus, tstep=sample(4:150,1), tp=neutraltraitsParam,age.threshold=sample(100,1), out=c("finalpop"),logging="",ma=1,traitsid=paste0("t",1:z))
+                                      quickV=modelVector(K=K, m=1, b=0, r=0, rho=1, d=0, maturity=sample(100,1), endrepro=sample(100,1), population=population, comus=initcomus, tstep=sample(4:150,1), tp=neutraltraitsParam,age.threshold=sample(100,1), out=c("finalpop"),logging="",ma=1,traitsid=)
 
                                       testthat::expect_true(all(apply(quickV$population[,traitsid],2,sum)== apply(population[,traitsid],2,sum) ))
                                       testthat::expect_true(nrow(population)== nrow(population))
@@ -107,7 +107,8 @@ testthat::test_that("model when only social learning changes but no grow",{
 
                                       N=200
                                       population=cbind(newpop(N,age="random",community = communities),initNeutralTraits(N,z))
-                                      quickV=modelVector(K=K, m=1, b=0, r=0, rho=1, d=0, maturity=sample(100,1), endrepro=sample(100,1), population=population, comus=initcomus, tstep=sample(z:120,1), tp=neutraltraitsParam,age.threshold=sample(100,1), out=c("finalpop"),logging="",ma=1,traitsid=paste0("t",1:z))
+                                      population[1:50,"age"]=0
+                                      quickV=modelVector(K=K, m=1, b=0, r=0, rho=1, d=0, maturity=sample(100,1), endrepro=sample(100,1), population=population, comus=initcomus, tstep=sample(10:120,1), tp=neutraltraitsParam,age.threshold=sample(100,1), out=c("finalpop"),logging=c("",""),ma=1,traitsid=paste0("t",1:z))
                                       traitsid=paste0("t",1:z)
                                       testthat::expect_false(all(apply(quickV$population[,traitsid],2,sum)== apply(population[,traitsid],2,sum) ))
                                       testthat::expect_true(nrow(population)== nrow(population))
@@ -131,7 +132,7 @@ quickV=modelVector(K=K, m=1, b=0.01, r=0.005, rho=1, d=0.01, maturity=10, endrep
 testthat::expect_false(all(apply(quickV$population[,traitsid],2,sum)== apply(population[,traitsid],2,sum) ))
                                       testthat::expect_true(nrow(population)== nrow(population))
 
-testthat::test_that("model when random social learning, steady grow/devrease",{
+testthat::test_that("model when random social learning, steady grow/decrease",{
                         replicate(10,
                                   {
                                       z=sample(2:20,1)
@@ -174,10 +175,10 @@ testthat::test_that("model social learning,adaptive learning,...",
                                       N=K*percomu
                                       pos=random2Dgrid(K=K,Gx=100)
                                       a=initAdaptiveTraits(ki=ki,km=km,n=20 )
-                                      initcomus=initialiseCommunities(traits=a,coordinates=pos)
+                                      initcomus=initialiseCommunities(traits=a,coordinates=pos,G=100)
                                       initcomus$size=rep(percomu,K)
                                       communities=unlist(lapply(1:K,function(i)rep(i,initcomus$size[i])))
                                       population=cbind(newpop(N,age="random",community = communities),initNeutralTraits(N,z))
-                                      quickV=modelVector(K=K, m=1, b=0.07, r=0.005, rho=0, d=0.01, maturity=18, endrepro=65, population=population, comus=initcomus, tstep=sample(50:60,1), tp=neutraltraitsParam,age.threshold=20, out=c("finalpop"),logging=c(""),ma=1,traitsid=paste0("t",1:z))
+                                      quickV=modelVector(K=K, m=1, b=0.07, r=0.005, rho=0, d=0.01, maturity=18, endrepro=65, population=population, comus=initcomus, tstep=sample(50:60,1), tp=neutraltraitsParam,age.threshold=20, out=c("finalpop"),logging=c("time","visu"),ma=1,traitsid=paste0("t",1:z))
                                   })
 })
