f5ec1d615ff03d882576334447d3f7def3260cea
Simon <simon@elisya.org>
2024-01-07 16:12:03 +0000

    document data population exemple

--- R/data.R
+++ R/data.R
@@ -4,6 +4,17 @@
 #' Report ...
 #'
 #' @format ## `population`
-#' A data frame with 7,240 rows and 60 columns:
+#' A fake populaiton with valid structure (couple, families and ages)
+#' 600 row (individuals)  and 13 columns
+#' \describe{
+#'   \item{id}{unique id for every individual}
+#'   \item{cid}{unique id for each couple}
+#'   \item{fid}{unique id for each family}
+#'   \item{age}{age of individual}
+#'   \item{sex}{Sex of individual}
+#'   \item{partner}{id of partner}
+#'   \item{community}{Community of each individual}
+#'   \item{t1 ... t4}{Neutral traits fo each individual}
+#' }
 "population"
 
