8336ae4266fe7dce9c58fec032b640a7bfa7ce1d
Simon <simon@elisya.org>
2024-01-10 18:18:43 +0000

    add som tool functions

---
+++ R/tools.R
@@ -0,0 +1,45 @@
+shuffle <- function(x)x[sample.int(length(x))]
+
+print.pathways <- function(pathways,verb=T){
+    for(i in 1:nrow(pathways$pre)){
+        it=paste0("c",i)
+        for(tm in c("pre","post")){
+            pt=paste0(colnames(pathways[[tm]])[pathways[[tm]][i,]==1],collapse=",")
+            if(verb)it=paste0(it," ",tm,": ",pt)
+            else it=paste0(it," ",pt)
+        }
+        if(verb)it=paste0(it," sex:",pathways$s[i])
+        else it=paste0(it," ",pathways$s[i])
+        print(it)
+    }
+
+}
+
+print.pathways.latex<- function(pathways){
+    for(i in 1:nrow(pathways$pre)){
+        it=paste0("$c_{",i,"}$")
+        for(tm in c("pre","post")){
+            pt=paste0(colnames(pathways[[tm]])[pathways[[tm]][i,]==1],collapse="&")
+            it=paste0(it,"&",pt)
+        }
+        it=paste0(it,"&",pathways$s[i],"\\\\\n")
+        cat(it)
+    }
+
+}
+
+#for(tm in c("pre","post"))
+#  paste(tm,colnames(neutraltraitsParam[[tm]])[neutraltraitsParam[[tm]][i,]==1],sep=":")
+#}
+}
+
+resetIds <- function(population){
+    population[population[,"cid"]!= -1 ,"cid"] = as.numeric(as.factor(as.character(population[population[,"cid"]!= -1 ,"cid"])))
+    population[,"fid"] = as.numeric(as.factor(as.character(population[ ,"fid"])))
+    ids = as.numeric(as.factor(as.character(population[ ,"id"])))
+    names(ids)=population[,"id"]
+    population[,"id"] = as.numeric(as.factor(as.character(population[ ,"id"])))
+    population[,"partner"]=as.numeric(ids[as.character(population[,"partner"])])
+    population[is.na(population[,"partner"]),"partner"]=-1
+    population
+}
