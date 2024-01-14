062b4087fbc8a9c935c13b616d3e1b03b7ff19f4
Simon <simon@elisya.org>
2024-01-14 09:51:09 +0000

    exploraion script

--- inst/scripts/explore2populations.R
+++ inst/scripts/explore2populations.R
@@ -17,11 +17,17 @@ for(i in 1:z){population[population[,"community"]==i,traitsid[i]]=1}
 knitr::kable(aggregate(population[,traitsid],by=list(population[,"community"]),sum))
 population=population[,-c(12,13)]
 tstep=10000
-quickV=modelVector(K=ki, m=1, b=0.2, r=0.1, rho=0, d=0.01, maturity=18, endrepro=65, population=population, comus=initcomus, tstep=tstep, tp=neutraltraitsParam,age.threshold=20, out=c("finalpop","finalcomus","popsize"),logging=c("time"),ma=1,traitsid=paste0("t",1:z),F_Th = 150)
+initcomus$size=unname(table(population[,"community"]))
+
+a=Sys.time()
+quickV=modelVector(K=ki, m=1, b=0.2, r=0.1, rho=0, d=0.01, maturity=18, endrepro=65, population=population, comus=initcomus, tstep=tstep, tp=neutraltraitsParam,age.threshold=20, out=c("finalpop","finalcomus","popsize"),logging=c("done"),ma=1,traitsid=paste0("t",1:z),F_Th = 150)
+print(Sys.time()-a)
 
 tstep=10000
 rhos=c(0,.5,1)
-repli=40
+repli=30
+library(parallel)
+cl <- makeCluster(30,type="FORK",outfile="log.txt")
 rhodepSBBIB=lapply(rhos,function(rho){
            neutraltraitsParam$post[,]=0
            neutraltraitsParam$pre[,]=0
@@ -29,7 +35,8 @@ rhodepSBBIB=lapply(rhos,function(rho){
            allinlaw=parSapply(cl,1:repli,function(r)
                               {
                                   set.seed(as.numeric(Sys.time())+r)
-                                  quickV=modelVector(K=K, m=1, b=0.2, r=0.2, rho=rho, d=0.01, maturity=18, endrepro=65, population=population, comus=initcomus, tstep=tstep, tp=neutraltraitsParam,age.threshold=20, out=c("traitsumary","finalpop","finalcomus","popsize"),logging=c("time"),ma=1,traitsid=paste0("t",1:z),F_Th = 150,vidfile=NULL)
+                                  rho=rhos[1]
+                                  quickV=modelVector(K=K, m=1, b=0.2, r=0.1, rho=rho, d=0.01, maturity=18, endrepro=65, population=population, comus=initcomus, tstep=tstep, tp=neutraltraitsParam,age.threshold=20, out=c("traitsumary","finalpop","finalcomus","popsize"),logging=c("done"),ma=1,traitsid=paste0("t",1:z),F_Th = 150,vidfile=NULL)
                                   pop=quickV$population
                                   do.call("rbind",quickV$traitsumary)/quickV$popsize
                               },simplify=F)
@@ -37,7 +44,7 @@ rhodepSBBIB=lapply(rhos,function(rho){
            neutraltraitsParam$post[,"h"]=1
            allposthoriz=parSapply(cl,1:repli,function(r){
                                   set.seed(as.numeric(Sys.time())+r)
-                                      quickV=modelVector(K=K, m=1, b=0.2, r=0.1, rho=rho, d=0.01, maturity=18, endrepro=65, population=population, comus=initcomus, tstep=tstep, tp=neutraltraitsParam,age.threshold=20, out=c("traitsumary","finalpop","finalcomus","popsize"),logging=c("time","visu"),ma=1,traitsid=paste0("t",1:z),F_Th = 150,vidfile=NULL,beta=0)
+                                      quickV=modelVector(K=K, m=1, b=0.2, r=0.1, rho=rho, d=0.01, maturity=18, endrepro=65, population=population, comus=initcomus, tstep=tstep, tp=neutraltraitsParam,age.threshold=20, out=c("traitsumary","finalpop","finalcomus","popsize"),logging=c("done","visu"),ma=1,traitsid=paste0("t",1:z),F_Th = 150,vidfile=NULL,beta=0)
                                       pop=quickV$population
                                      do.call("rbind",quickV$traitsumary)/quickV$popsize
                               },simplify=F)
@@ -47,21 +54,28 @@ rhodepSBBIB=lapply(rhos,function(rho){
            neutraltraitsParam$pre[,"h"]=1
            allprehoriz=parSapply(cl,1:repli,function(r){
                                   set.seed(as.numeric(Sys.time())+r)
-                                     quickV=modelVector(K=K, m=1, b=0.2, r=0.1, rho=rho, d=0.01, maturity=18, endrepro=65, population=population, comus=initcomus, tstep=tstep, tp=neutraltraitsParam,age.threshold=20, out=c("traitsumary","finalpop","finalcomus","popsize"),logging=c("time"),ma=1,traitsid=paste0("t",1:z),F_Th = 150,vidfile=NULL)
+                                     quickV=modelVector(K=K, m=1, b=0.2, r=0.1, rho=rho, d=0.01, maturity=18, endrepro=65, population=population, comus=initcomus, tstep=tstep, tp=neutraltraitsParam,age.threshold=20, out=c("traitsumary","finalpop","finalcomus","popsize"),logging=c("done"),ma=1,traitsid=paste0("t",1:z),F_Th = 150,vidfile=NULL)
                                      pop=quickV$population
                                      do.call("rbind",quickV$traitsumary)/quickV$popsize
                               },simplify=F)
            neutraltraitsParam$pre[,"h"]=0
            neutraltraitsParam$post[,"o"]=1
-           allposthoriz=parSapply(cl,1:repli,function(r){
+           allpostoblic=parSapply(cl,1:repli,function(r){
                                   set.seed(as.numeric(Sys.time())+r)
-                                     quickV=modelVector(K=K, m=1, b=0.2, r=0.1, rho=rho, d=0.01, maturity=18, endrepro=65, population=population, comus=initcomus, tstep=tstep, tp=neutraltraitsParam,age.threshold=20, out=c("traitsumary","finalpop","finalcomus","popsize"),logging=c("time"),ma=1,traitsid=paste0("t",1:z),F_Th = 150,vidfile=NULL)
+                                     quickV=modelVector(K=K, m=1, b=0.2, r=0.1, rho=rho, d=0.01, maturity=18, endrepro=65, population=population, comus=initcomus, tstep=tstep, tp=neutraltraitsParam,age.threshold=20, out=c("traitsumary","finalpop","finalcomus","popsize"),logging=c("done"),ma=1,traitsid=paste0("t",1:z),F_Th = 150,vidfile=NULL)
                                      pop=quickV$population
                                      do.call("rbind",quickV$traitsumary)/quickV$popsize
                               },simplify=F)
-           list(allinlaw=allinlaw,allposthoriz=allposthoriz,allprehoriz=allprehoriz)
+           neutraltraitsParam$pre[,"o"]=1
+           allprepostoblic=parSapply(cl,1:repli,function(r){
+                                  set.seed(as.numeric(Sys.time())+r)
+                                     quickV=modelVector(K=K, m=1, b=0.2, r=0.1, rho=rho, d=0.01, maturity=18, endrepro=65, population=population, comus=initcomus, tstep=tstep, tp=neutraltraitsParam,age.threshold=20, out=c("traitsumary","finalpop","finalcomus","popsize"),logging=c("done"),ma=1,traitsid=paste0("t",1:z),F_Th = 150,vidfile=NULL)
+                                     pop=quickV$population
+                                     do.call("rbind",quickV$traitsumary)/quickV$popsize
+                              },simplify=F)
+           list(allinlaw=allinlaw,allposthoriz=allposthoriz,allprehoriz=allprehoriz,allprepostoblic=allprepostoblic,allpostoblic=allpostoblic)
 }
 )
 
 stopCluster(cl)
-
+saveRDS(file="10k.RDS",rhodepSBBIB)
