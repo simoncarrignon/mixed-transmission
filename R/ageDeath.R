a943e66bb76e13f7de725aa7e543e93f305d235d
Enrico Crema <enrico.crema@gmail.com>
2024-01-16 16:49:55 +0000

    Added age-structured mortality function

---
+++ R/ageDeath.R
@@ -0,0 +1,16 @@
+
+# x ... vector of ages
+# b ... start age of specific mortality, last value ranges to infinity
+# m ... mortality per bracket
+
+# x  <- round(runif(100,min=0,max=100))
+# b  <- c(0,5,18,40,65,85)
+# m  <- c(0.15,0.01,0.01,0.02,0.05,1)
+# ageDeath(x,b,m)
+
+ageDeath <- function(x,b,m)
+{
+	b <- cbind(b,c(b[-1],Inf))
+	rnd <- runif(length(x))
+	return(rnd<sapply(x,function(x,b,m){m[which(b[,1]<=x & b[,2]>x)]},b=b,m=m))
+}
