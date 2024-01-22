0a63847fc736003de9003f4d99314850823363ec
Simon <simon@elisya.org>
2024-01-22 08:05:24 +0000

    auto explo bonus beta rho --small

--- inst/scripts/neutralTranmission.R
+++ inst/scripts/neutralTranmission.R
@@ -65,21 +65,24 @@ allpopsizesonly=parSapply(cl,1:200,function(b){
 stopCluster(cl)
 saveRDS(file=file.path(expname,"neutralTraits_longer.RDS"),allpopsizesonly)
 
-for(beta in c(-10,0,.1)){
-    for(bonus in 0:3){
-        expname=paste0("AdapEvoLSingleBR500kRHO_1_G10_bonus_",bonus,"_beta_",beta)
-        dir.create(expname)
-        initcomus$adaptivetraits[c(1,2),]=1
-        cl<-makeCluster(40,type="FORK",outfile=file.path(expname,"log.txt"))
-        allpopsizesonly=parSapply(cl,1:200,function(b){
-                                      set.seed(as.numeric(Sys.time())+b)
-                                      tryCatch({
-                                          singlesimu=modelVector(K=K, m=1, b=0.216, r=0.005*bonus, rho=1, d=mortality, maturity=18, endrepro=65, population=population, comus=initcomus, tstep=500, tp=fullpathways,age.threshold=20, out=c("popsize","finalpop","finalcomus","traitsumary"),logging=c("done"),ma=1,traitsid=traitsid,F_Th=100,testdebug=F,fracfiss=.5,beta=beta)
-                                          saveRDS(file=file.path(expname,paste0("singlesimu_s_",b,".RDS")),singlesimu)
-                                          singlesimu$popsize
-                                      },error=function(e){ print("problem ======");print(e)})
+for(rho in c(0.5,1)){
+    for(beta in c(-10,0,.1)){
+        for(bonus in 0:3){
+            expname=paste0("TraitTraj_500t_RHO_",rho,"_G10_bonus_",bonus,"_beta_",beta)
+            dir.create(expname)
+            initcomus$adaptivetraits[c(1,2),]=1
+            cl<-makeCluster(55,type="FORK",outfile=file.path(expname,"log.txt"))
+            allpopsizesonly=parSapply(cl,56:200,function(b){
+                                          set.seed(as.numeric(Sys.time())+b)
+                                          tryCatch({
+                                              singlesimu=modelVector(K=K, m=1, b=0.216, r=0.005*bonus, rho=rho, d=mortality, maturity=18, endrepro=65, population=population, comus=initcomus, tstep=500, tp=fullpathways,age.threshold=20, out=c("popsize","finalpop","finalcomus","traitsumary","comusize","traitpercomu"),logging=c("done"),ma=1,traitsid=traitsid,F_Th=100,testdebug=F,fracfiss=.5,beta=beta)
+                                              singlesimu$popsize
+                                              saveRDS(file=file.path(expname,paste0("singlesimu_s_",b,".RDS")),singlesimu)
+
+                                          },error=function(e){ print("problem ======");print(e)})
 })
-        stopCluster(cl)
-        saveRDS(file=file.path(expname,"neutralTraits_longer.RDS"),allpopsizesonly)
+            stopCluster(cl)
+            saveRDS(file=file.path(expname,"neutralTraits_longer.RDS"),allpopsizesonly)
+        }
     }
 }
