3fa7a9205d626046edd097da9dd917e1172d4cc2
Simon <simon@elisya.org>
2024-02-06 18:14:37 +0000

    attempt to run simu with new varents

---
+++ inst/scripts/base_exp_newPW.R
@@ -0,0 +1,74 @@
+devtools::load_all()
+ki=10
+km=0
+G=10
+z=2
+neutraltraitsParam=generatePathways(z = z)
+traitsid=paste0("t",1:z)
+K=km+ki
+pos=random2Dgrid(K=K,Gx=G)
+percomu=50
+a=initAdaptiveTraits(ki=ki,km=km,n=3)
+initcomus=initialiseCommunities(ki=ki,km=km,traits=a,coordinates=pos,G=G,plot=T,sizes=percomu)
+initcomus$occupation[,]=1
+communities=unlist(lapply(1:K,function(i)rep(i,initcomus$size[i])))
+agescat=c(0,5,18,40,65,85)
+mortality=c(0.15,0.01,0.01,0.02,0.05,1)
+agdis=sample(seq_along(agescat),size=K*percomu,prob=1-mortality,replace=T)
+getagdis=sapply(agdis,function(i)sample(agescat[i]:agescat[i+1],1))
+population=cbind(newpop(length(getagdis),age=getagdis,community = sample(1:K,size=length(getagdis),replace=T)),generateTraitsMatrix(length(getagdis),z))
+initrun=modelVector(K=K, m=1, b=0.42, r=0, rho=.5, d=c(0.15,0.01,0.01,0.02,0.05,1), maturity=18, endrepro=65, population=population, comus=initcomus, tstep=500, tp=generatePathways(z=z),age.threshold=20, out=c("popsize","finalpop","repros","finalcomus"),logging=c("done","","time"),ma=1,traitsid=paste0("t",1:z),F_Th = 75,popcapsize=20*70,fracfiss=.25)
+population=resetIds(initrun$population)
+population=paperpopulation
+initcomus=initialiseCommunities(ki=ki,km=km,traits=a,coordinates=pos,G=G,plot=T,sizes=unname(table(population[,"community"])))
+
+
+initcomus$adaptivetraits[c(1,2),]=1
+z=45
+fullpathways=generatePathways(z = z)
+
+pw=1
+for(sb in c(0,.5,1)){
+    print(pw)
+    for(pre in c("v","h","o")){
+        fullpathways$pre[pw:(pw+3),pre]=1
+        fullpathways$s[pw:(pw+3)]=sb
+        pw=pw+1
+        print(pw)
+        fullpathways$s[pw]=sb
+        for(tr in c(.1,.9)){
+            for(post in c("h","o")){
+                fullpathways$post[pw,post]=1
+                print(pw)
+                fullpathways$s[pw]=sb
+                fullpathways$tr[pw]=tr
+                pw=pw+1
+            }
+        }
+    }
+}
+traitsid=paste0("t",1:z)
+alltraits=generateTraitsMatrix(nrow(population),z)
+population=cbind(population[,-c(10,11)],alltraits)
+for(rho in c(1)){
+    for(beta in c(.1)){
+        for(bonus in 0:2){
+            expname=paste0("NewPW_TraitTraj_10t_RHO_",rho,"_G10_bonus_",bonus,"_beta_",beta)
+            dir.create(expname)
+            initcomus$adaptivetraits[c(1,2),]=1
+            cl<-makeCluster(10,type="FORK",outfile=file.path(expname,"log.txt"))
+            allpopsizesonly=parSapply(cl,1:10,function(b){
+                                          set.seed(as.numeric(Sys.time())+b)
+                                          tryCatch({
+                                              singlesimu=modelVector(K=K, m=1, b=0.216, r=0.005*bonus, rho=rho, d=mortality, maturity=18, endrepro=65, population=population, comus=initcomus, tstep=10, tp=fullpathways,age.threshold=20, out=c("popsize","finalpop","finalcomus","traitsumary","comusize","traitpercomu"),logging=c("done"),ma=1,traitsid=traitsid,F_Th=100,testdebug=F,fracfiss=.5,beta=beta)
+                                              singlesimu$popsize
+                                              saveRDS(file=file.path(expname,paste0("singlesimu_s_",b,".RDS")),singlesimu)
+
+                                          },error=function(e){ print("problem ======");print(e)})
+})
+            stopCluster(cl)
+            saveRDS(file=file.path(expname,"neutralTraits_longer.RDS"),allpopsizesonly)
+        }
+    }
+}
+
