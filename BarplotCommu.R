b7f5b3b213c0dcfd0affc80fbbf31665ad7086bf
Simon <simon@elisya.org>
2024-02-14 21:33:02 +0000

    more visualisation tools

--- BarplotCommu.R
+++ BarplotCommu.R
@@ -6,7 +6,7 @@ cols3=colorRampPalette(c("#006400","#FFD700"))(3)
 par(mfrow=c(3,3),mar=c(0,0,0,0),oma=c(3,3,3,3),xpd=T)
 
 for(bonus in c(0,1,3)){
-    for(beta in c(-10,0,0.1)){
+    for(beta in c(-10,0,)){
         expname=paste0("NewPW_invertSex_TraitTraj_10t_RHO_0_G10_bonus_",bonus,"_beta_",beta)
         allsingle.exp <- list.files(expname,pattern = "si.*\\.RDS",full.names = TRUE)
         cl<-makeCluster(6,type="FORK",outfile="log.txt")
@@ -16,16 +16,113 @@ for(bonus in c(0,1,3)){
                              #alltypes[alltypes==2]=1
                              sapply(1:500,function(t){
                                         sizes_t=one$comusize[[t]]
-                                        table(factor(alltypes[1:length(sizes_t)],level=c(0,1,2,3)))/length(sizes_t)
+                                        table(factor(alltypes[1:length(sizes_t)],level=c(0,1,2,3)))#/length(sizes_t)
 })
 })
         stopCluster(cl)
 
         allstrats=sapply(1:4,function(strat)apply(sapply(allrep,function(i)i[strat,]),1,mean))
         #allmedian=sapply(allstrats,function(i)i[3,])
-    barplot(t(allstrats),border=NA,space=0,xlab="time",ylab="mean proportion of each strats",col=cols4,ann=F,axes=F)#,main=paste0("One Simulation wiht param:\n",expname))
-    if(beta==-10){ axis(2,cex=.8) ; mtext("mean proportion of each strats",2,1.8,cex=.8)}
+    barplot(t(allstrats),border=NA,space=0,xlab="time",ylab="mean number of community with each strats",col=cols4,ann=F,axes=F,ylim=c(0,100))#,main=paste0("One Simulation wiht param:\n",expname))
+    if(beta==-10){ axis(2,cex=.8) ; mtext("mean number of communities",2,1.8,cex=.8)}
     if(bonus==3) { axis(1,cex=.8) ; mtext("time",1,1.8,cex=.8)}
     if(bonus==0) { mtext(bquote(beta==.(beta)),3,1,cex=.8)}
     if(beta==0) { mtext(bquote(bonus==.(bonus)),4,1,cex=.8)}
 }}
+
+
+#4 STRATS relative frequencies
+par(mfrow=c(3,2),mar=c(0,0,0,0),oma=c(3,3,3,3),xpd=T)
+
+for(bonus in c(0,1,3)){
+    for(beta in c(-10,0)){
+        expname=paste0("NewPW_TraitTraj_StratTraj_500ts_RHO_0_G10_bonus_",bonus,"_beta_",beta)
+        allsingle.exp <- list.files(expname,pattern = "si.*\\.RDS",full.names = TRUE)
+        cl<-makeCluster(6,type="FORK",outfile="log.txt")
+        allrep=parLapply(cl,allsingle.exp,function(totes){
+                             one=readRDS(totes)
+                             alltypes=getCommuType(one)
+                             sapply(1:500,function(t){
+                                        sizes_t=one$comusize[[t]]
+                                        ada_time=one$traitpercomu[[t]][,c("a1","a2","a3")]
+                                        alltypes=apply(ada_time/sizes_t,1,sum)
+                                        table(factor(alltypes[1:length(sizes_t)],level=c(0,1,2,3)))/length(sizes_t)
+
+})
+})
+        stopCluster(cl)
+
+        allstrats=sapply(1:4,function(strat)apply(sapply(allrep,function(i)i[strat,]),1,mean))
+        #allmedian=sapply(allstrats,function(i)i[3,])
+    barplot(t(allstrats),border=NA,space=0,xlab="time",ylab="mean number of community with each strats",col=cols4,ann=F,axes=F)#,main=paste0("One Simulation wiht param:\n",expname))
+    if(beta==-10){ axis(2,cex=.8) ; mtext("mean number of communities",2,1.8,cex=.8)}
+    if(bonus==3) { axis(1,cex=.8) ; mtext("time",1,1.8,cex=.8)}
+    if(bonus==0) { mtext(bquote(beta==.(beta)),3,1,cex=.8)}
+    if(beta==0) { mtext(bquote(bonus==.(bonus)),4,1,cex=.8)}
+}}
+
+
+#4 STRATS counts
+par(mfrow=c(3,2),mar=c(0,0,0,0),oma=c(3,3,3,3),xpd=T)
+
+for(bonus in c(0,1,3)){
+    for(beta in c(-10,0)){
+        expname=paste0("NewPW_TraitTraj_StratTraj_500ts_RHO_0_G10_bonus_",bonus,"_beta_",beta)
+        allsingle.exp <- list.files(expname,pattern = "si.*\\.RDS",full.names = TRUE)
+        cl<-makeCluster(6,type="FORK",outfile="log.txt")
+        allrep=parLapply(cl,allsingle.exp,function(totes){
+                             one=readRDS(totes)
+                             alltypes=getCommuType(one)
+                             sapply(1:500,function(t){
+                                        sizes_t=one$comusize[[t]]
+                                        ada_time=one$traitpercomu[[t]][,c("a1","a2","a3")]
+                                        alltypes=apply(ada_time/sizes_t,1,sum)
+                                        table(factor(alltypes[1:length(sizes_t)],level=c(0,1,2,3)))
+
+})
+})
+        stopCluster(cl)
+
+        allstrats=sapply(1:4,function(strat)apply(sapply(allrep,function(i)i[strat,]),1,mean))
+        #allmedian=sapply(allstrats,function(i)i[3,])
+    barplot(t(allstrats),border=NA,space=0,xlab="time",ylab="mean number of community with each strats",col=cols4,ann=F,axes=F)#,main=paste0("One Simulation wiht param:\n",expname))
+    if(beta==-10){ axis(2,cex=.8) ; mtext("mean number of communities",2,1.8,cex=.8)}
+    if(bonus==3) { axis(1,cex=.8) ; mtext("time",1,1.8,cex=.8)}
+    if(bonus==0) { mtext(bquote(beta==.(beta)),3,1,cex=.8)}
+    if(beta==0) { mtext(bquote(bonus==.(bonus)),4,1,cex=.8)}
+}}
+
+#4 STRATS counts
+
+    for(beta in c(-10,0)){
+
+par(mfrow=c(3,2),mar=c(0,0,0,0),oma=c(3,3,3,3),xpd=T)
+for(bonus in c(0,1,3)){
+        expname=paste0("NewPW_TraitTraj_StratTraj_500ts_RHO_0_G10_bonus_",bonus,"_beta_",beta)
+        allsingle.exp <- list.files(expname,pattern = "si.*\\.RDS",full.names = TRUE)
+for(type in c(0,1)){
+        cl<-makeCluster(6,type="FORK",outfile="log.txt")
+        allrep=parLapply(cl,allsingle.exp,function(totes){
+                             one=readRDS(totes)
+                             sapply(1:500,function(t){
+                                        sizes_t=one$comusize[[t]]
+                                        ada_time=one$traitpercomu[[t]][,c("a1","a2","a3")]
+                                        alltypes=apply(ada_time/sizes_t,1,sum)
+                                        alltypes=alltypes[ one$finalcomus$strat[1:length(sizes_t)]==type]
+                                        table(factor(alltypes,level=c(0,1,2,3)))#/sum(one$finalcomus$strat[1:length(sizes_t)]==type)
+
+})
+})
+        stopCluster(cl)
+
+        allstrats=sapply(1:4,function(strat)apply(sapply(allrep,function(i)i[strat,]),1,mean))
+        #allmedian=sapply(allstrats,function(i)i[3,])
+    barplot(t(allstrats),border=NA,space=0,xlab="time",ylab="mean number of community with each strats",col=cols4,ann=F,axes=F,ylim=c(0,100))#,main=paste0("One Simulation wiht param:\n",expname))
+}
+    if(beta==-10){ axis(2,cex=.8) ; mtext("mean number of communities",2,1.8,cex=.8)}
+    if(bonus==3) { axis(1,cex=.8) ; mtext("time",1,1.8,cex=.8)}
+    if(bonus==0) { mtext(bquote(beta==.(beta)),3,1,cex=.8)}
+    if(beta==0) { mtext(bquote(bonus==.(bonus)),4,1,cex=.8)}
+}
+dev.new()
+}
--- R/analysis_tools.R
+++ R/analysis_tools.R
@@ -10,3 +10,44 @@ getCommuType <- function(exp){
                sum(exp$traitpercomu[[st]][com,paste0("a",1:3)]/comusize[[st]][com])
     })
 }
+
+#using a the result of a simulation
+#return the type of comu (0,1,2,3) 
+getCountBySexAndCommu <- function(population,pathways){
+    traitsid=paste0("t",seq_along(pathways$s))
+    freqcom=c()
+    wholecomus=sort(unique(population[,"community"])) 
+    for(s in c(0,1)){
+        single.bias=traitsid[pathways$s == s]
+        singlesex=population[population[,"sex"]==s,]
+        singlesex.comus=factor(singlesex[,"community"],levels=wholecomus)
+        freqcom=cbind(freqcom,xtabs(singlesex[,single.bias] ~ singlesex.comus)/as.numeric(table(singlesex.comus)))
+    }       
+    single.bias=traitsid[pathways$s == 0.5]
+    freqcom=cbind(freqcom,xtabs(population[,single.bias] ~ population[,"community"])/as.numeric(table(population[,"community"])))
+    freqcom[,traitsid]
+}
+
+extractResults <- function(expname,pathways=pathways,traitsel=NULL){
+    allsingle.exp <- list.files(expname,pattern = "si.*\\.RDS",full.names = TRUE)
+    alltraitComu=lapply(allsingle.exp,function(expfname){
+                            print(expfname)
+                            one=readRDS(expfname)
+                            population=NULL
+                            if(is.null(one$popwhenfull))
+                                population=one$population
+                            else
+                                population=one$popwhenfull
+                            getCountBySexAndCommu(population,pathways)
+    })
+    lapply(traitsel,function(ti) unlist(lapply(alltraitComu,function(singex)singex[,ti])))
+}
+
+
+getDensities <- function(traits,bw=0.05){
+    if(is.null(traits))return(NULL)
+    traits.dens=density(traits,na.rm=T,from=0,to=1,bw=bw)
+    y=c(0,traits.dens$y,0)
+    x=c(0,traits.dens$x,1)
+    return(cbind(x,y/max(y)))
+}
