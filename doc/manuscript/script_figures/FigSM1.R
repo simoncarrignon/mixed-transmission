04ecb341e3698a83cabe5c78212ff538f3293444
Simon <simon@elisya.org>
2024-07-11 19:45:19 +0200

    updated script for Figure SM 1

--- doc/manuscript/script_figures/FigSM1.R
+++ doc/manuscript/script_figures/FigSM1.R
@@ -1,8 +1,9 @@
-##ECPLORE BONUS AND BETAjj
+##EXPLORE BONUS AND BETA suplementary material, FigS1 two panels
 devtools::load_all()
+setwd(file.path(here::here(),"simulations"))
 
-#Left Panel GROWTH RATE only (NO BONUS)
-expname=here::here("simulations",paste0("NewAges_ExploringBirthrateOnGrowth_5000_FS"))
+#Left Panel impact of base birth rate (b) on measured grwoth rates (NO BONUS)
+expname="NewAges_ExploringBirthrateOnGrowth_5000_FS"
 params=readRDS(paste0(expname,"_params.RDS"))
 birthrate=params[,1]
 allpopsizesonly=sapply(1:nrow(params),function(expr){
@@ -19,11 +20,7 @@ if(file.exists(expfname)){
 else{ c(NULL,NULL,NULL) }
 })
 
-##this 1000 are more simulatino, available on throughton, should be dl
-allpopsizesonly1000=readRDS(here::here("simulations","NewAges_ExploringBonusOnGrowth_1000_500ts.RDS"))
-allpopsizesonly1000=do.call("cbind",allpopsizesonly1000[lengths(allpopsizesonly1000)==4])
-allpopsizesonly=cbind(allpopsizesonly,allpopsizesonly1000)
-setwd(file.path(here::here(),"simulations"))
+#Right Panel: Bonus (f) on measured grwoth rates 
 prefixexp="BonusExploNewAges_5000_FS_poppaper2"
 params=readRDS(paste0(prefixexp,"_params.RDS"))
 expname=prefixexp
@@ -42,16 +39,23 @@ bonusbeta=sapply(1:nrow(params),function(expr){
 })
 bonusbeta=do.call(cbind,bonusbeta) # concat all 
 
-bgpe=10
+##this 1000 are more simulatino, available on throughton, should be dl
+bonusbeta1000=readRDS("NewAges_ExploringBonusOnGrowth_1000_500ts.RDS")
+bonusbeta1000=do.call("cbind",bonusbeta1000[lengths(bonusbeta1000)==4])
+bonusbeta=cbind(bonusbeta,bonusbeta1000)
+
+bgpe=5
 custom_colors <- rev(colorRampPalette(c("red", "white", "blue"))(bgpe))
 
+#png(file="growth_rate_wrt_b_f.png",height=850,width=1450,pointsize=22,type="cairo") #for the exact png parameters
 
-png(file="growth_rate_wrt_b_f.png",height=850,width=1450,pointsize=22,type="cairo")
 par(mfrow=c(1,2))
 par(mar=c(4,6,4,0))
-allpopsizesonlyadj=allpopsizesonly[,(allpopsizesonly["br",]/.67)>0.25]
+
+allpopsizesonlyadj=allpopsizesonly[,(allpopsizesonly["br",]/.67)>0.25] #adjust birthrate that was wrongly used in the original parameters
+
 plot(allpopsizesonlyadj["br",]/0.67,allpopsizesonlyadj["effect",],ylim=c(-0.02,0.025),xlim=c(0.25,0.5),xlab="birth rate",ylab=expression(frac(N[t+1]-N[t],N[t])),main="")
-#,xlab="f",ylab=expression(abline(v=0.322,col="red")
+
 abline(v=0.3224,col="black",lwd=3)
 abline(v=0.3224,col="green",lwd=1.5)
 par(mar=c(4,3,4,1))
@@ -59,6 +63,6 @@ plot(bonusbeta["bonus",],bonusbeta["effect",],ylab="" ,xlab="f",pch=21,main="eff
 abline(v=c(0,0.005,0.015),col="black",lwd=2.5)
 abline(v=c(0,0.005,0.015),col="green",lwd=1)
 legend("bottomright",fill=custom_colors,title="beta",legend=levels(cut(bonusbeta["beta",],breaks=bgpe)),bty="n",cex=.8)
-dev.off()
 
+#dev.off()
 
