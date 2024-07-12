8febbb6fb608a1f1fdcfc729cfffab07554d9835
Simon <simon@elisya.org>
2024-07-12 10:33:35 +0200

    add data collection to heatmaps FigS6 generations

--- doc/manuscript/script_figures/FigSM6.R
+++ doc/manuscript/script_figures/FigSM6.R
@@ -1,3 +1,45 @@
+library(parallel)
+sexcols=c('#EE5A45', '#D4D6B9','#1E8F89')
+names(sexcols)=c(0,.5,1)
+
+devtools::load_all()
+setwd(file.path(here::here(),"simulations"))
+expname=paste0("BonusExploNewAges_11550_FS_poppaperNewRanges_rho0")
+bonus=round(seq(0,0.02,length.out =11),digits=3)
+beta=round(seq(-3,1,length.out=21),digits=3)
+params= do.call("rbind",lapply(1:50,function(i)expand.grid(bonus,beta)))
+allres=matrix(NA,nrow=length(beta),ncol=length(bonus))
+colnames(allres)=as.character(bonus)
+rownames(allres)=as.character(beta)
+
+cl<-makeCluster(11,type="FORK",outfile="log.txt")
+allallres=list()
+allallcol=list()
+allexpe=list(C1=c(1,16,31),
+             C2=c(1,16,31)+1,
+             C3=c(1,16,31)+2,
+             C4=c(1,16,31)+3,
+             C5=c(1,16,31)+4)
+for(ne in names(allexpe)){
+    for(traitsel in allexpe[[ne]]){
+        allres[,]=NA
+        for(be in beta){
+            print(paste(ne,be,traitsel))
+            cur=parLapply(cl,bonus,function(bo,be,traitsel){
+
+                singlep=which(params[,1]==bo & params[,2]==be)
+                uu=extractResults(expname,traitsel=traitsel,pathways=fullpathways,diffexp=T,params=singlep,log=T,type=T)[[2]]
+            } ,be=be,traitsel=traitsel)
+            allres[as.character(be),]=sapply(cur,function(i)mean(i[[1]],na.rm=T))
+        }
+        allallres[[paste0(ne,"_distribFBETA_pbias",fullpathways$s[traitsel])]]=allres
+        allallcol[[paste0(ne,"_distribFBETA_pbias",fullpathways$s[traitsel])]]=sexcols[as.character(fullpathways$s[traitsel])]
+    }
+}
+stopCluster(cl)
+
+save(file="allmatricsTrait.RDS",allallres)
+
 allres=readRDS(file="allmatricsTrait.RDS")
 
 
